85d249ceb8ae8b94616c31dc5ac88f91d547fc8f
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-05-25 21:38:25 +0700

    Tiles module added

--- src/app.tsx
+++ src/app.tsx
@@ -5,11 +5,13 @@ import Header from "./modules/header/Header";
 import Main from "./modules/main-container/main-container";
 import Reminder from "./modules/reminder/reminder";
 import TabsGroup from "./components/tabs/tabs-group";
+import Tiles from "./modules/tiles/tiles";
 
 // =====================================================
 
 import reminders from "./mocks/reminders";
 import { timeTabs } from "./mocks/tabs";
+import { expencesData, fuelData, runData } from "./mocks/charts";
 
 // ===========================================
 
@@ -24,6 +26,7 @@ export default function App() {
 			<Main>
 				<Reminder reminders={reminders} />
 				<TabsGroup name="time-interval" tabs={timeTabs} tabGroupChangedHandler={timeTabClicked} />
+				<Tiles runData={runData} fuelData={fuelData} expencesData={expencesData} />
 			</Main>
 		</>
 	);
---
+++ src/modules/tiles/tiles.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Tile from "../../components/tile/tile";
+import { TileChartData } from "../../components/tile/tile.types";
+
+import styles from "./tiles.module.scss";
+
+type TilesProps = {
+	runData: TileChartData;
+	fuelData: TileChartData;
+	expencesData: TileChartData;
+};
+
+export default function Tiles({ runData, fuelData, expencesData }: TilesProps) {
+	return (
+		<section className={styles.tiles}>
+			<Tile title="Пробег" units="км." value={2700} chartData={runData} />
+			<Tile title="Расход топлива" units="л./100 км." value={8.2} chartData={fuelData} />
+			<Tile title="Затраты" units="руб." value={1200} chartData={expencesData} />
+		</section>
+	);
+}
