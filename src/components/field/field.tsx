3f1313c2fbf6202f60c1a5fc787fa730c8a067bf
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-05-27 17:36:01 +0700

    Field component refactor

--- src/components/field/field.tsx
+++ src/components/field/field.tsx
@@ -1,24 +1,27 @@
 import * as React from "react";
 import classNames from "classnames";
 import styles from "./field.module.scss";
+import textStyles from "../../styles/typography.module.scss";
 
 type FieldProps = {
 	name: string;
+	label: string;
+	auxStyles?: string;
 	mode?: "text" | "decimal";
 };
 
-export default function Field({ name, mode = "text" }: FieldProps) {
+export default function Field({ name, label, auxStyles, mode = "text" }: FieldProps) {
 	return (
-		<div className={classNames(styles.field, `field-${name}`)}>
+		<div className={classNames(styles.field, auxStyles)}>
 			<input
-				className={styles.input}
+				className={classNames(styles.input, textStyles.titleNormal)}
 				type="text"
 				name={name}
 				inputMode={mode}
 				placeholder={mode === "text" ? " " : undefined}
 			/>
-			<label className={styles.label} htmlFor={name}>
-				Дата
+			<label className={classNames(styles.label, textStyles.titleNormal)} htmlFor={name}>
+				{label}
 			</label>
 		</div>
 	);
