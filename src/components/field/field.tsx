027d1bb883ba7e42fc003adbdbb80122ce80d8e7
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-05-30 19:30:15 +0700

    Field component adds units after value on blur event

--- src/components/field/field.tsx
+++ src/components/field/field.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
 import styles from "./field.module.scss";
 import textStyles from "../../styles/typography.module.scss";
@@ -8,17 +8,26 @@ type FieldProps = {
 	label: string;
 	auxStyles?: string;
 	numeric?: boolean;
+	units?: string;
 };
 
-export default function Field({ name, label, auxStyles, numeric = false }: FieldProps) {
+export default function Field({ name, label, auxStyles, numeric = false, units = "" }: FieldProps) {
+	const [value, setValue] = useState("");
+	const setUnits = (e:any) => setValue(`${e.target.value} ${units}`);
+	const removeUnits = (e:any) => setValue(e.target.value.replace(` ${units}`, ""));
+
 	return (
 		<div className={classNames(styles.field, auxStyles)}>
 			<input
 				className={classNames(styles.input, textStyles.titleNormal)}
 				type="text"
 				name={name}
+				value={value}
 				inputMode={numeric ? "decimal" : "text"}
 				placeholder={" "}
+				onChange={(e) => setValue(e.target.value)}
+				onBlur={units ? setUnits : undefined}
+				onFocus={units ? removeUnits : undefined}
 			/>
 			<label className={classNames(styles.label, textStyles.titleNormal)} htmlFor={name}>
 				{label}
--- src/modules/form-fuel/form-fuel.tsx
+++ src/modules/form-fuel/form-fuel.tsx
@@ -7,10 +7,10 @@ export default function FormFuel() {
 	return (
 		<div className={styles.fuelFields}>
 			<Field name="date" label="Дата" auxStyles={styles.date} />
-			<Field name="run" label="Пробег" auxStyles={styles.run} numeric/>
-			<Field name="cost" label="Стоимость" auxStyles={styles.cost} numeric/>
-			<Field name="price" label="Цена за литр" auxStyles={styles.price} numeric/>
-			<Field name="volume" label="Объем" auxStyles={styles.volume} numeric/>
+			<Field name="run" label="Пробег" auxStyles={styles.run} units="км." numeric />
+			<Field name="cost" label="Стоимость" auxStyles={styles.cost} units="руб." numeric />
+			<Field name="price" label="Цена за литр" auxStyles={styles.price} units="руб." numeric />
+			<Field name="volume" label="Объем" auxStyles={styles.volume} units="л." numeric />
 		</div>
 	);
 }
