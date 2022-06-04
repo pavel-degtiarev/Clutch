b49b78529029a74f60fb543849dc033d117a7937
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-06-04 13:08:31 +0700

    FormFuel wrapped into WithVerifySubmit HOC

--- src/app.tsx
+++ src/app.tsx
@@ -8,7 +8,10 @@ import TabsGroup from "./components/tabs/tabs-group";
 import Tiles from "./modules/tiles/tiles";
 import PopupSwitch from "./components/popup-switch/popup-switch";
 import { FormItem } from "./components/popup-switch/popup-switch.types";
-import FormFuel from "./modules/form-fuel/form-fuel";
+import WithVerifySubmit from "./HOC/with-validate-submit/with-validate-submit";
+import FormFuel, { FuelFormFields, FuelFormState } from "./modules/form-fuel/form-fuel";
+import validateFuelForm from "./modules/form-fuel/form-fuel-validation";
+import submitFuelForm from "./modules/form-fuel/form-fuel-submit";
 import FormSpare from "./modules/form-spare/form-spare";
 import FormOther from "./modules/form-other/form-other";
 
@@ -26,16 +29,22 @@ dayjs.locale("ru");
 const forms: FormItem[] = [
 	{
 		title: "Топливо",
-		form: <FormFuel />,
+		form: (
+			<WithVerifySubmit<FuelFormFields, FuelFormState>
+				Form={FormFuel}
+				validate={validateFuelForm}
+				submit={submitFuelForm}
+			/>
+		),
 	},
-	{
-		title: "Расходники, запчасти",
-		form: <FormSpare />,
-	},
-	{
-		title: "Прочее",
-		form: <FormOther />,
-	},
+	// {
+	// 	title: "Расходники, запчасти",
+	// 	form: <FormSpare />,
+	// },
+	// {
+	// 	title: "Прочее",
+	// 	form: <FormOther />,
+	// },
 
 	// { title: "Сервис", callback: () => console.log("Сервис") },
 ];
