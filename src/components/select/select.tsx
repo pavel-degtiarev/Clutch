2c690448a81914e01c8d6c7bfdda68fcaddae7ad
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-06-07 12:34:19 +0700

    disabled state added to Select

--- src/components/select/select.tsx
+++ src/components/select/select.tsx
@@ -11,11 +11,14 @@ type SelectProps = {
 	name: string;
 	options: SelectOption[];
 	selected: SelectOption;
+	disabled?: boolean;
 };
 
-export default function Select({ name, options, selected }: SelectProps) {
+export default function Select({ name, options, selected, disabled=false }: SelectProps) {
 	return (
-		<select className={styles.select} name={name} id={name} defaultValue={selected.value}>
+		<select className={styles.select} name={name} id={name}
+			defaultValue={selected.value} disabled={disabled}>
+			
 			{options.map((item, index) => (
 				<option key={index} value={item.value}>
 					{item.label}
