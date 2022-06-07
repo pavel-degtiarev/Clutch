608292c93a3ed7b6c0d0a7410b56f344ae68c6b4
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-06-07 11:38:57 +0700

    Checkbox component added

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import classNames from "classnames";
 
 import styles from "./checkbox.module.scss";
@@ -7,16 +7,23 @@ import textStyles from "../../styles/typography.module.scss";
 type CheckboxProps = {
 	name: string;
 	label: string;
+	isChecked: boolean;
 	auxStyles?: string;
 	children?: ReactNode;
 };
 
-export default function Checkbox({ name, label, auxStyles, children = null }: CheckboxProps) {
-	const id = `${name}`; // TODO Need generate unique ID
+export default function Checkbox({
+	name, label, auxStyles, isChecked = false, children = null }: CheckboxProps) {
+	
+	const [checkboxChecked, setCheckboxChecked] = useState(isChecked);
+	const id = `${name}`;
 
 	return (
 		<div className={classNames(styles.checkbox, auxStyles)}>
-			<input className={styles.input} type="checkbox" id={id} name={name} />
+			<input className={styles.input} type="checkbox"
+				id={id} name={name} checked={checkboxChecked}
+				onChange={() => setCheckboxChecked((prevState) => !prevState)}
+			/>
 			<label className={classNames(styles.label, textStyles.titleNormal)} htmlFor={name}>
 				{label}
 			</label>
