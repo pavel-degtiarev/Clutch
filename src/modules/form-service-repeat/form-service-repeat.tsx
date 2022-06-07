7c5ba9e738341996d4128f1a97b220b02484f3fa
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-06-07 12:35:38 +0700

    Form state changed according Checkbox state

--- src/modules/form-service-repeat/form-service-repeat.tsx
+++ src/modules/form-service-repeat/form-service-repeat.tsx
@@ -10,7 +10,9 @@ import Checkbox from "../../components/checkbox/checkbox";
 import Field from "../../components/field/field";
 import FieldGroup from "../../components/field-group/field-group";
 import Select from "../../components/select/select";
-import { TimelUnit } from "../../../global.var";
+import { FormUnits, TimelUnit } from "../../../global.var";
+import Button from "../../components/button/button";
+import { collectFormFields } from "../../utilities/collect-form-fields";
 
 const timeSlotOptions = [
 	{
@@ -37,37 +39,73 @@ export type ServiceRepeatFormState = typeof ServiceRepeatFormInitState;
 export type ServiceRepeatFormFields = keyof ServiceRepeatFormState;
 
 export default function FormServiceRepeat({
-  validate, submit }: FormComponentProps<ServiceRepeatFormFields, ServiceRepeatFormState>) {
-  
+	validate,
+	submit,
+}: FormComponentProps<ServiceRepeatFormFields, ServiceRepeatFormState>) {
 	const [formState, setFormState] = useState<ServiceRepeatFormState>(ServiceRepeatFormInitState);
 	const formRef = useRef({} as HTMLFormElement);
 
 	return (
-		<div className={containerStyles.popupContent}>
-			<form
-				className={containerStyles.form}
-				ref={formRef}
-				onChange={(e) => {
-					const name = (e.target as HTMLInputElement).name as ServiceRepeatFormFields;
-					const value = (e.target as HTMLInputElement).value;
-					validate(name, value, setFormState as setStateFunction<ServiceRepeatFormState>);
-				}}>
-				<div className={styles.serviceRepeatFields}>
-					<FieldGroup>
-						<Checkbox name="repeat-by-run" label="по пробегу" isChecked={formState.repeatByRun} />
-						<Field name="total" label="Пробег" value={formState.run} />
-					</FieldGroup>
-
-					<FieldGroup>
-						<Checkbox name="repeat-by-time" label="по времени" isChecked={formState.repeatByTime} />
-
-						<FieldGroup horizontal>
-							<Field name="time-amount" label="Время" value={formState.run} />
-							<Select name="time-slot" options={timeSlotOptions} selected={timeSlotOptions[1]} />
+		<>
+			<div className={containerStyles.popupContent}>
+				<form
+					className={containerStyles.form}
+					ref={formRef}
+					onChange={(e) => {
+						const name = (e.target as HTMLInputElement).name as ServiceRepeatFormFields;
+						const value = (e.target as HTMLInputElement).value;
+						validate(name, value, setFormState as setStateFunction<ServiceRepeatFormState>);
+					}}>
+					<div className={styles.serviceRepeatFields}>
+						<FieldGroup>
+							<Checkbox name="repeat-by-run" label="по пробегу"
+								isChecked={formState.repeatByRun}
+								callback={() =>
+									setFormState((prevState) => {
+										return { ...prevState, repeatByRun: !prevState.repeatByRun };
+									})
+								}
+							/>
+							<Field name="repeated-run" label="Пробег"
+								value={formState.run} numeric units={FormUnits.RUN}
+								disabled={!formState.repeatByRun}
+							/>
 						</FieldGroup>
-					</FieldGroup>
-				</div>
-			</form>
-		</div>
+
+						<FieldGroup>
+							<Checkbox name="repeat-by-time" label="по времени"
+								isChecked={formState.repeatByTime}
+								callback={() =>
+									setFormState((prevState) => {
+										return { ...prevState, repeatByTime: !prevState.repeatByTime };
+									})
+								}
+							/>
+
+							<FieldGroup horizontal>
+								<Field name="repeated-time" label="Время"
+									value={formState.run}
+									disabled={!formState.repeatByTime}
+								/>
+								<Select name="repeat-time-slot" options={timeSlotOptions}
+									selected={timeSlotOptions[1]}
+									disabled={!formState.repeatByTime}
+								/>
+							</FieldGroup>
+						</FieldGroup>
+					</div>
+				</form>
+			</div>
+
+			<Button
+				title="Сохранить"
+				auxStyles={containerStyles.saveButton}
+				clickHandler={() => {
+					if (submit(collectFormFields<ServiceRepeatFormFields>(formRef.current))) {
+						// dispatch(popupClosed());
+					}
+				}}
+			/>
+		</>
 	);
 }
