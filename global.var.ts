d1f0d922a7e39efb19a43d466318c58011715cc2
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-06-08 11:28:33 +0700

    TimeSuffixes added

--- global.var.ts
+++ global.var.ts
@@ -4,6 +4,18 @@ export enum TimeUnits {
 	YEARS = "years",
 }
 
+export enum TimeSuffixes {
+	ONE_DAY = "день",
+	TWO_TO_FOUR_DAYS = "дня",
+	MANY_DAYS = "дней",
+	ONE_MONTH = "месяц",
+	TWO_TO_FOUR_MONTHS = "месяца",
+	MANY_MONTHS = "месяцев",
+	ONE_YEAR = "год",
+	TWO_TO_FOUR_YEARS = "года",
+	MANY_YEARS = "лет",
+}
+
 export enum YearSuffix {
 	SINGLE,
 	MULTIPLE,
