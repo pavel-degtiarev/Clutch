252ca9aaf82f5922f8d1bd80d3602ce58ec4ea6c
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-06-09 13:15:08 +0700

    FieldWithSuffix component added

---
+++ src/components/field/field-with-suffix.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import FieldContainer from "./field-container";
+import { FieldSuffixes, TimeSuffixes } from "../../../global.var";
+import Label from "./label";
+import { addSuffix } from "../../utilities/units";
+import { FieldProps } from "./field-types";
+import { InputNumericProps } from "./input-numeric";
+import { InputDecimalProps } from "./input-decimal";
+
+interface FieldWithSuffix extends FieldProps {
+	InputComponent: (props: InputNumericProps | InputDecimalProps) => JSX.Element;
+	suffix: FieldSuffixes | TimeSuffixes;
+}
+
+export default function FieldWithSuffix({
+	InputComponent, name, label, value, suffix, auxStyles, disabled, children }: FieldWithSuffix) {
+	
+	const [isEditing, setEditing] = useState(false);
+	const [currValue, setCurrValue] = useState(value);
+
+	return (
+		<FieldContainer auxStyles={auxStyles}>
+			{isEditing ? (
+				<InputComponent
+					name={name} value={currValue} disabled={disabled}
+					blurHandler={(e) => {
+						setCurrValue(e.target.value);
+						setEditing(!isEditing);
+					}}
+				/>
+			) : (
+				<InputComponent
+					name={name+"2"} value={addSuffix(currValue, suffix)} disabled={disabled}
+					focusHandler={() => setEditing(!isEditing)}
+				/>
+			)}
+
+			<Label inputName={name} label={label} />
+
+			{children}
+		</FieldContainer>
+	);
+}
--- src/modules/form-service-repeat/form-service-repeat.tsx
+++ src/modules/form-service-repeat/form-service-repeat.tsx
@@ -3,9 +3,6 @@ import {
 	FormComponentProps,
 	setStateFunction,
 } from "../../HOC/with-validate-submit/with-validate-submit";
-
-import styles from "./form-service-repeat.module.scss";
-import containerStyles from "../../components/popup-container/popup-container.module.scss";
 import Checkbox from "../../components/checkbox/checkbox";
 import Field from "../../components/field/field";
 import FieldGroup from "../../components/field-group/field-group";
@@ -13,6 +10,11 @@ import Select from "../../components/select/select";
 import { FieldSuffixes, TimeUnits } from "../../../global.var";
 import Button from "../../components/button/button";
 import { collectFormFields } from "../../utilities/collect-form-fields";
+import FieldWithSuffix from "../../components/field/field-with-suffix";
+
+import styles from "./form-service-repeat.module.scss";
+import containerStyles from "../../components/popup-container/popup-container.module.scss";
+import InputNumeric from "../../components/field/input-numeric";
 
 const timeSlotOptions = [
 	{
@@ -58,37 +60,38 @@ export default function FormServiceRepeat({
 					}}>
 					<div className={styles.serviceRepeatFields}>
 						<FieldGroup>
-							<Checkbox name="repeat-by-run" label="по пробегу"
-								isChecked={formState.repeatByRun}
+							<Checkbox
+								name="repeat-by-run" label="по пробегу" isChecked={formState.repeatByRun}
 								callback={() =>
 									setFormState((prevState) => {
 										return { ...prevState, repeatByRun: !prevState.repeatByRun };
 									})
 								}
 							/>
-							<Field name="repeated-run" label="Пробег"
-								value={formState.run} numeric suffix={FieldSuffixes.RUN}
+							<FieldWithSuffix
+								InputComponent={InputNumeric}
+								name="repeated-run" label="Пробег" value={formState.run}
+								suffix={FieldSuffixes.RUN}
 								disabled={!formState.repeatByRun}
 							/>
 						</FieldGroup>
 
 						<FieldGroup>
-							<Checkbox name="repeat-by-time" label="по времени"
-								isChecked={formState.repeatByTime}
+							<Checkbox
+								name="repeat-by-time" label="по времени" isChecked={formState.repeatByTime}
 								callback={() =>
 									setFormState((prevState) => {
 										return { ...prevState, repeatByTime: !prevState.repeatByTime };
 									})
 								}
 							/>
-
 							<FieldGroup horizontal>
-								<Field name="repeated-time" label="Время"
-									value={formState.run}
+								<Field
+									name="repeated-time" label="Время" value={formState.run}
 									disabled={!formState.repeatByTime}
 								/>
-								<Select name="repeat-time-slot" options={timeSlotOptions}
-									selected={timeSlotOptions[1]}
+								<Select
+									name="repeat-time-slot" options={timeSlotOptions} selected={timeSlotOptions[1]}
 									disabled={!formState.repeatByTime}
 								/>
 							</FieldGroup>
