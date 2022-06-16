71d42d2acbd0e03f997ccac45d5af7c868c9a230
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-06-16 22:01:39 +0700

    Types turned into Generics

--- global.d.ts
+++ global.d.ts
@@ -6,7 +6,7 @@ declare module "*.svg";
 type clickHandler = (event: React.MouseEvent<HTMLElement>) => void;
 
 type FormValidations<T extends string> = {
-	[key in T]?: (value: string) => void;
+	[key in T]?: (value: string | boolean) => void;
 };
 
 type FormFields<T extends string> = { [key in T]: string };
--- src/components/field/field-types.ts
+++ src/components/field/field-types.ts
@@ -7,7 +7,7 @@ export enum InputType {
 	TEXT = "text",
 	NUMBER = "number",
 	DATE = "date",
-	CHECKBOX = "checkbox"
+	CHECKBOX = "checkbox",
 }
 
 export enum InputMode {
@@ -20,16 +20,16 @@ export interface WithChildren {
 	children?: ReactNode;
 }
 
-export interface ControlledField {
-	changeHandler?: (value: string) => string;
+export interface ControlledField<T extends string | boolean> {
+	changeHandler?: (value: T) => T;
 	focusHandler?: (arg: any) => void;
 	blurHandler?: (arg: any) => void;
 }
 
-export interface FieldProps extends
-    Pick<InputProps, "name" | "value" | "disabled">,
+export interface FieldProps
+	extends Pick<InputProps, "name" | "value" | "disabled">,
 		Pick<LabelProps, "label">,
-		ControlledField,
+		ControlledField<string>,
 		WithChildren {
 	auxStyles?: string;
 }
