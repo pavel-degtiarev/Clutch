6cffd193385e4e0db763d32643aa467404844f87
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-06-16 22:07:46 +0700

    FormService validation added

--- src/modules/form-service/form-service-validation.ts
+++ src/modules/form-service/form-service-validation.ts
@@ -3,6 +3,47 @@ import { ServiceFormFields, ServiceFormState } from "./form-service";
 
 export default function getValidateServiceForm(setState: setStateFunction<ServiceFormState>) {
 	return function (target: ServiceFormFields, value: string) {
+
+		const validations: FormValidations<ServiceFormFields> = {
+			serviceDate: (value) => {
+				setState((prevState) => {
+					const newState = { ...prevState, serviceDate: value };
+					return newState;
+				});
+			},
+
+			serviceDescription: (value) => {
+				setState((prevState) => {
+					const newState = { ...prevState, serviceDescription: value };
+					return newState;
+				});
+			},
+
+			serviceRun: (value) => {
+				setState((prevState) => {
+					const newState = { ...prevState, serviceRun: value };
+					return newState;
+				});
+			},
+
+			serviceTotal: (value) => {
+				setState((prevState) => {
+					const newState = { ...prevState, serviceTotal: value };
+					return newState;
+				});
+			},
+
+			serviceRepeat: (value) => {
+				setState((prevState) => {
+					const newState = { ...prevState, serviceRepeat: value };
+					return newState;
+				});
+			},
+		};
+		
 		console.log("Service form validation", target, value);
+
+		const checkTarget = validations[target];
+		checkTarget && checkTarget(value);
 	};
 }
