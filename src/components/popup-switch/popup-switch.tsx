6c734ae337437a483b8a32f183599f4a4e9e9598
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-06-29 17:45:42 +0700

    in PopupSwitch added PopupContainer for subforms

--- src/components/popup-switch/popup-switch.tsx
+++ src/components/popup-switch/popup-switch.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, Dispatch, useReducer } from "react";
 import ButtonRollup from "../../modules/button-rollup/button-rollup";
 import PopupContainer from "../popup-container/popup-container";
+import { formClosed, subformClosed } from "./popup-switch-actions";
 import { initState, reducer } from "./popup-switch-reducer";
 import { Action, FormItem } from "./popup-switch.types";
 
@@ -12,19 +13,31 @@ export const DispatchContext = createContext<Dispatch<Action>>(() => {});
 
 export default function PopupSwitch({ forms }: PopupSwitchProps) {
 	const [state, dispatch] = useReducer(reducer, initState);
+	const subformOpened = !!state.currentSubform;
 
 	return (
 		<DispatchContext.Provider value={dispatch}>
+
 			<ButtonRollup
 				title="Потратить деньги"
 				forms={forms}
 				rollupOpened={state.rollupOpened}
 			/>
+
 			<PopupContainer
-				opened={state.popupOpened}
-				title={state.currentPopup?.title}
-				form={state.currentPopup?.form}
+				title={state.currentForm?.title}
+				form={state.currentForm?.form}
+				inactive={subformOpened}
+				closeAction={formClosed}
 			/>
+
+			<PopupContainer
+				title={state.currentSubform?.title}
+				form={state.currentSubform?.form}
+				closeAction={subformClosed}
+				small
+			/>
+
 		</DispatchContext.Provider>
 	);
 }
