e159b4a8d8ea3b36bc3db62d3cfd0216248505d2
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-07-10 20:45:59 +0700

    RowDeletable toggles DeleteDetails button on swipe

--- src/components/details-list/row-deletable.tsx
+++ src/components/details-list/row-deletable.tsx
@@ -1,18 +1,60 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, TouchEvent, useCallback, useRef, useState } from "react";
 import ButtonIcon from "../button-icon/button-icon";
 import styles from "./details-list.module.scss";
 
 interface RowProps {
-  children: ReactNode;
-  deleteHandler: () => void;
+	children: ReactNode;
+	deleteHandler: () => void;
 }
 
+const MAX_DISTANCE = 40;
+
 export function RowDeletable({ children, deleteHandler }: RowProps) {
+	const ref = useRef<HTMLDivElement>(null);
+	const [startPoint, setStartPoint] = useState(0);
+	const [buttonVisible, setButtonVisible] = useState(false);
+
+	const onTouchStart = useCallback((e: TouchEvent) => {
+		setStartPoint(e.changedTouches[0].pageX);
+	}, []);
+
+	const onTouchMove = useCallback((e: TouchEvent) => {
+		if (!ref.current) return;
+
+		const distance = e.changedTouches[0].pageX - startPoint;
+
+		if (Math.abs(distance) <= MAX_DISTANCE) {
+			ref.current.style.left = `${distance}px`;
+			return;
+		}
+
+		if (distance < 0) {
+			setButtonVisible(true);
+		}
+
+		if (distance > 0 && buttonVisible) {
+			setButtonVisible(false);
+		}
+	}, [ref, startPoint]);
+
+	const onTouchEnd = useCallback(() => {
+		if (!ref.current) return;
+		ref.current.style.left = "";
+	}, [ref]);
+
 	return (
-		<div className={styles.row} draggable="true">
+		<div
+			className={styles.row}
+			draggable="true"
+			ref={ref}
+			onTouchStart={onTouchStart}
+			onTouchMove={onTouchMove}
+			onTouchEnd={onTouchEnd}>
 			{children}
 
-			<ButtonIcon auxClassNames={styles.deleteRowButton} handler={() => deleteHandler()} />
+			{buttonVisible && (
+				<ButtonIcon auxClassNames={styles.deleteRowButton} handler={() => deleteHandler()} />
+			)}
 		</div>
 	);
 }
