9c64d579e046cb227fa478799a79fe9a40b7b5a5
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-07-12 12:02:40 +0700

    formOtherSubmit uses generic submitForm

--- src/modules/form-other/form-other-submit.ts
+++ src/modules/form-other/form-other-submit.ts
@@ -1,6 +1,14 @@
-import { OtherFormFields } from "./form-other";
+import submitForm, { OtherFormFinalState } from "../../utilities/submit-form";
+import { OtherFormState } from "./form-other";
+import { convertOtherFields } from "./form-other-convert-fields";
+import { formOtherCheckpoints } from "./form-other-submit-checkpoints";
 
-export default function submitOtherForm(formFields: FormFields<OtherFormFields>): boolean {
-	console.log("Send FormOther data to API", formFields);
-	return true;
+export default function submitOtherForm(state: OtherFormState): boolean {
+	console.log("Send FormOther data to API");
+
+	return submitForm<OtherFormState, OtherFormFinalState>(
+		state,
+		formOtherCheckpoints,
+		convertOtherFields
+	);
 }
