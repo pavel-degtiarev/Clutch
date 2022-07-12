cd122dd55c6a5671737ee0a65d030d8da2e6903e
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-07-12 15:48:05 +0700

    SubmitForm new form types added

--- src/utilities/submit-form.ts
+++ src/utilities/submit-form.ts
@@ -2,16 +2,29 @@ import { TargetFormState } from "../HOC/with-validate-submit/with-validate-submi
 import { FuelFormFields } from "../modules/form-fuel/form-fuel";
 
 export type FuelFormFinalState = { [key in FuelFormFields]: number };
-export type OtherFormFinalState = { otherDate: number, otherTitle: string, otherPrice: number};
-export type SpareFormFinalState = { spareDate: number, spareTitle: string, sparePrice: number};
+export type OtherFormFinalState = { otherDate: number; otherTitle: string; otherPrice: number };
+export type SpareFormFinalState = { spareDate: number; spareTitle: string; sparePrice: number };
+export type ServiceRepeatFormFinalState = {
+	repeatByRun: boolean;
+	repeatingRun: number;
+	repeatByTime: boolean;
+	repeatingTime: number;
+	repeatTimeSlot: string;
+};
 
-export type FinalFormState = FuelFormFinalState | OtherFormFinalState | SpareFormFinalState;
+export type FinalFormState =
+	| FuelFormFinalState
+	| OtherFormFinalState
+	| SpareFormFinalState
+	| ServiceRepeatFormFinalState;
 
 // ==============================================
 
 export default function submitForm<T extends TargetFormState, V extends FinalFormState>(
-  state: T, checkpoints: Checkpoint<T>[], convertFields: (state: T) => V): boolean {
-  
+	state: T,
+	checkpoints: Checkpoint<T>[],
+	convertFields: (state: T) => V
+): boolean {
 	for (const checkpoint of checkpoints) {
 		if (!checkpoint(state)) return false;
 	}
