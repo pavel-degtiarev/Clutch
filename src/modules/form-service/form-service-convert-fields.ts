ea2ccc0b6fe3b59a92241a01075c0f3d0b681969
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-07-18 19:56:55 +0700

    FormServiceConvertFields refactored

--- src/modules/form-service/form-service-convert-fields.ts
+++ src/modules/form-service/form-service-convert-fields.ts
@@ -1,13 +1,18 @@
 import dayjs from "dayjs";
+import { ServiceFormFields, ServiceFormState } from "../../store/form-init-states";
 import { ServiceFormFinalState } from "../../utilities/submit-form";
-import { ServiceFormFields, ServiceFormState } from "./form-service";
 
 export function convertServiceFields(state: ServiceFormState): ServiceFormFinalState {
-	const tempState: any = { ...state, fuelDate: String(dayjs(state.serviceDate).toDate().valueOf()) };
-	Object.keys(tempState).forEach(
-		(key) => (tempState[key as ServiceFormFields] = Number(tempState[key as ServiceFormFields]))
-	);
-
-	const finalState: ServiceFormFinalState = tempState;
+	const finalState: ServiceFormFinalState = {
+		serviceDate: dayjs(state.serviceDate).toDate().valueOf(),
+		serviceDescription: state.serviceDescription,
+		serviceRun: Number(state.serviceRun),
+		serviceTotal: Number(state.serviceTotal),
+		serviceRepeat: state.serviceRepeat,
+		serviceDetails: {
+			repeat: null,
+			details: null,
+		},
+	};
 	return finalState;
 }
