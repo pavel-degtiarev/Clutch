1bcb242c07eaa44ef32cd82cf169c97d57a553b0
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-07-18 16:20:28 +0700

    saveToDB returns transaction result

--- src/API/save.ts
+++ src/API/save.ts
@@ -2,14 +2,23 @@ import { FinalFormState } from "../utilities/submit-form";
 import { getDB } from "./init-db";
 
 export default async function saveToDb(
-	store: string,
-	value: FinalFormState,
-	// onSuccess: Function
-): Promise<void> {
+  store: string, value: FinalFormState, onSuccess?: () => void): Promise<boolean> {
+  
 	const db = getDB();
+	if (!db) throw new Error("DB access error");
+
 	const transaction = db.transaction(store, "readwrite");
 
-	const result = await transaction.store.add(value);
-
-	console.log(result);
+	await transaction.store.add(value);
+	const result = await transaction.done.then(
+		() => {
+			onSuccess && onSuccess();
+			return true;
+		},
+		() => {
+			return false;
+		}
+  );
+  
+  return result;
 }
