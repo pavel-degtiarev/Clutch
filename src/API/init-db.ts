e1ca45196e430cf926ca37f6cc36fce7f9a8cdaa
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-07-18 16:19:15 +0700

    DB indexes created

--- src/API/init-db.ts
+++ src/API/init-db.ts
@@ -6,18 +6,33 @@ export const dbNames = {
 	FUEL: "fuel",
 	SERVICE: "service",
 	SPARE: "spare",
-	OTHER:"other"
-}
+	OTHER: "other",
+};
 
 export async function initClutchDB() {
 	clutchDB = await openDB("clutchDB", 1, {
 		upgrade(db) {
-			db.createObjectStore(dbNames.FUEL, { keyPath: "id", autoIncrement: true });
-			db.createObjectStore(dbNames.SERVICE, { keyPath: "id", autoIncrement: true });
-			db.createObjectStore(dbNames.SPARE, { keyPath: "id", autoIncrement: true });
-			db.createObjectStore(dbNames.OTHER, { keyPath: "id", autoIncrement: true });
+			db.createObjectStore(dbNames.FUEL, { keyPath: "id", autoIncrement: true }).createIndex(
+				"date",
+				"fuelDate"
+			);
+
+			db.createObjectStore(dbNames.SERVICE, { keyPath: "id", autoIncrement: true }).createIndex(
+				"date",
+				"serviceDate"
+			);
+
+			db.createObjectStore(dbNames.SPARE, { keyPath: "id", autoIncrement: true }).createIndex(
+				"date",
+				"spareDate"
+			);
+			
+			db.createObjectStore(dbNames.OTHER, { keyPath: "id", autoIncrement: true }).createIndex(
+				"date",
+				"otherDate"
+			);
 		},
-	});	
+	});
 }
 
 export function getDB() {
