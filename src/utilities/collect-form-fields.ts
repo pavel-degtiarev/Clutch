30c4bc55e7508313b0c5a7ea9768d1d1403698c8
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-07-18 20:29:58 +0700

    redundant file removed

--- src/utilities/collect-form-fields.ts
+++
@@ -1,9 +0,0 @@
-import { removeSuffix } from "./units";
-
-export function collectFormFields<T extends string>(formRef: HTMLFormElement) {
-	const form = new FormData(formRef);
-	const formFields: FormFields<T> = {} as FormFields<T>;
-
-	[...form].forEach(([key, value]) => (formFields[key as T] = removeSuffix(value)));
-	return formFields;
-}
