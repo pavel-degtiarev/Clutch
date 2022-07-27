5e3ffbfa95ed85255fb92f583d3faf3c9702c4a0
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-07-27 20:29:04 +0700

    typo

--- webpack.config.js
+++ webpack.config.js
@@ -26,170 +26,170 @@ const isDevMode = process.env.NODE_ENV === "development";
 const isProdMode = !isDevMode;
 
 function fileName(ext = "[ext]") {
-	return isDevMode ? `[name]${ext}` : `[name]-[contenthash:10]${ext}`;
+  return isDevMode ? `[name]${ext}` : `[name]-[contenthash:10]${ext}`;
 }
 
 function generateSourceMap() {
-	return isDevMode ? "inline-source-map" : false;
+  return isDevMode ? "inline-source-map" : false;
 }
 
 function optimizationOptions(prodMode, chunkLibs = false) {
-	const options = {
-		minimize: prodMode,
-		minimizer: [
-			"...",
-			new CssMinimizerWebpackPlugin(),
-			new ImageMinimizerPlugin({
-				minimizer: {
-					implementation: ImageMinimizerPlugin.squooshMinify,
-				},
-			}),
-		],
-		splitChunks: {
-			cacheGroups: {
-				vendor: {
-					test: /[\\/]node_modules[\\/]/,
-					name: "vendor",
-					chunks: "all",
-					reuseExistingChunk: true,
-				},
-			},
-		},
-	};
+  const options = {
+    minimize: prodMode,
+    minimizer: [
+      "...",
+      new CssMinimizerWebpackPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.squooshMinify,
+        },
+      }),
+    ],
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendor",
+          chunks: "all",
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  };
 
-	if (chunkLibs) {
-		options.splitChunks.cacheGroups.lib = {
-			test: path.resolve(__dirname, customLibs),
-			name: "lib",
-			enforce: true,
-			chunks: "all",
-			reuseExistingChunk: true,
-		};
-	}
+  if (chunkLibs) {
+    options.splitChunks.cacheGroups.lib = {
+      test: path.resolve(__dirname, customLibs),
+      name: "lib",
+      enforce: true,
+      chunks: "all",
+      reuseExistingChunk: true,
+    };
+  }
 
-	return options;
+  return options;
 }
 
 function cssLoaders(moreLoaders = null) {
-	const loaders = [
-		MiniCssExtractPlugin.loader,
-		{
-			loader: "css-loader",
-			options: {
-				modules: {
-					auto: /\.module\.\w+$/i, //  filename.module.ext
-					localIdentName: isDevMode ? "[name]__[local]" : "[hash:base64]",
-				},
-			},
-		},
-		{
-			loader: "postcss-loader",
-			options: {
-				postcssOptions: {
-					plugins: ["postcss-preset-env"],
-				},
-			},
-		},
-	];
+  const loaders = [
+    MiniCssExtractPlugin.loader,
+    {
+      loader: "css-loader",
+      options: {
+        modules: {
+          auto: /\.module\.\w+$/i, //  filename.module.ext
+          localIdentName: isDevMode ? "[name]__[local]" : "[hash:base64]",
+        },
+      },
+    },
+    {
+      loader: "postcss-loader",
+      options: {
+        postcssOptions: {
+          plugins: ["postcss-preset-env"],
+        },
+      },
+    },
+  ];
 
-	if (moreLoaders) loaders.push(moreLoaders);
-	return loaders;
+  if (moreLoaders) loaders.push(moreLoaders);
+  return loaders;
 }
 
 function jsLoaders(morePresets = null) {
-	const opts = {
-		loader: "babel-loader",
-		options: {
-			presets: ["@babel/preset-env"],
-			plugins: [["@babel/transform-runtime"]],
-		},
-	};
+  const opts = {
+    loader: "babel-loader",
+    options: {
+      presets: ["@babel/preset-env"],
+      plugins: [["@babel/transform-runtime"]],
+    },
+  };
 
-	if (morePresets) opts.options.presets.push(morePresets);
-	return opts;
+  if (morePresets) opts.options.presets.push(morePresets);
+  return opts;
 }
 
 function devServerOptions() {
-	return {
-		static: { directory: path.resolve(__dirname, targetFolder), watch: true },
-		client: { logging: "error", overlay: false },
-		open: { app: { name: "Google Chrome" } },
-		historyApiFallback: true,
-	};
+  return {
+    static: { directory: path.resolve(__dirname, targetFolder), watch: true },
+    client: { logging: "error", overlay: false },
+    open: { app: { name: "Google Chrome" } },
+    historyApiFallback: true,
+  };
 }
 
 // =====================================================================
 
 module.exports = {
-	entry: { bundle: entryPoint },
-	output: {
-		filename: fileName(".js"),
-		path: path.resolve(__dirname, targetFolder),
-		clean: isProdMode,
-	},
+  entry: { bundle: entryPoint },
+  output: {
+    filename: fileName(".js"),
+    path: path.resolve(__dirname, targetFolder),
+    clean: isProdMode,
+  },
 
-	resolve: {
-		extensions: [".jsx", ".tsx", ".ts", "..."],
-		alias: {
-			node_modules: path.resolve(__dirname, "./node_modules/"),
-			markup: path.resolve(__dirname, "./markup/"),
-			src: path.resolve(__dirname, "./src/"),
-		},
-	},
+  resolve: {
+    extensions: [".jsx", ".tsx", ".ts", "..."],
+    alias: {
+      node_modules: path.resolve(__dirname, "./node_modules/"),
+      markup: path.resolve(__dirname, "./markup/"),
+      src: path.resolve(__dirname, "./src/"),
+    },
+  },
 
-	devtool: generateSourceMap(),
-	optimization: optimizationOptions(isProdMode, chunkCustomLibs),
-	devServer: devServerOptions(),
+  devtool: generateSourceMap(),
+  optimization: optimizationOptions(isProdMode, chunkCustomLibs),
+  devServer: devServerOptions(),
 
-	performance: {
-		maxEntrypointSize: 1024000,
-		maxAssetSize: 1024000,
-	},
+  performance: {
+    maxEntrypointSize: 1024000,
+    maxAssetSize: 1024000,
+  },
 
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: jsLoaders(),
-			},
-			{
-				test: /\.jsx$/,
-				exclude: /node_modules/,
-				use: jsLoaders("@babel/preset-react"),
-			},
-			{
-				test: /\.(ts|tsx)$/,
-				exclude: /node_modules/,
-				use: jsLoaders("@babel/preset-typescript"),
-			},
-			{
-				test: /\.css$/,
-				use: cssLoaders(),
-			},
-			{
-				test: /\.scss$/,
-				use: cssLoaders("sass-loader"),
-			},
-			{
-				test: /\.html$/,
-				use: ["html-loader"],
-			},
-			{
-				test: /\.(webp|jpg|jpeg|png|svg|ico)$/,
-				type: "asset/resource",
-				generator: { filename: `${targetImagesFolder}/${fileName()}` },
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				type: "asset/resource",
-				generator: { filename: `${targetFontsFolder}/${fileName()}` },
-			},
-		],
-	},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: jsLoaders("@babel/preset-react"),
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: jsLoaders("@babel/preset-typescript"),
+      },
+      {
+        test: /\.css$/,
+        use: cssLoaders(),
+      },
+      {
+        test: /\.scss$/,
+        use: cssLoaders("sass-loader"),
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(webp|jpg|jpeg|png|svg|ico)$/,
+        type: "asset/resource",
+        generator: { filename: `${targetImagesFolder}/${fileName()}` },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: "asset/resource",
+        generator: { filename: `${targetFontsFolder}/${fileName()}` },
+      },
+    ],
+  },
 
-	plugins: [
-		new HtmlWebpackPlugin({ template: htmlTemplate }),
-		new MiniCssExtractPlugin({ filename: `css/${fileName(".css")}` }),
-	],
+  plugins: [
+    new HtmlWebpackPlugin({ template: htmlTemplate }),
+    new MiniCssExtractPlugin({ filename: `css/${fileName(".css")}` }),
+  ],
 };
