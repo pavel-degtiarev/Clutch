faf4e58b3472dbc5e265c60882a54fe5f1558e91
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-07-27 22:46:28 +0700

    TileChart calcs normalized data then draws it

--- src/components/tile/tile-chart.tsx
+++ src/components/tile/tile-chart.tsx
@@ -4,26 +4,52 @@ import classNames from "classnames";
 import tileStyles from "./tile.module.scss";
 import styles from "./tile-chart.module.scss";
 
-type TileChartProps = {
+// ==================================
+
+const MAX_HEIGHT = 100;
+const MIN_HEIGHT = 30;
+const HEIGHT_DIFF = MAX_HEIGHT - MIN_HEIGHT;
+
+interface ChartBarProps {
+  value: number;
+}
+
+function ChartBar({ value }: ChartBarProps) {
+  return <div className={classNames(styles.bar)} style={{ height: `${value}%` }}></div>;
+}
+
+// ==================================
+
+interface TileChartProps {
   chartData: number[];
-};
+}
 
 export default function TileChart({ chartData }: TileChartProps) {
-  console.log(chartData);
-  
+
+  const normalizedData = calcNormalized(chartData);
+
   return (
     <div className={classNames(tileStyles.data, styles.chart)}>
       <div className={classNames(styles.chartContainer)}>
         <div className={classNames(styles.barsContainer)}>
-          <div className={classNames(styles.bar)} style={{ height: "65%" }}></div>
-          <div className={classNames(styles.bar)} style={{ height: "40%" }}></div>
-          <div className={classNames(styles.bar)} style={{ height: "0%" }}></div>
-          <div className={classNames(styles.bar)} style={{ height: "20%" }}></div>
-          <div className={classNames(styles.bar)} style={{ height: "80%" }}></div>
-          <div className={classNames(styles.bar)} style={{ height: "100%" }}></div>
-          <div className={classNames(styles.bar)} style={{ height: "85%" }}></div>
+          {normalizedData.map((item, index) => (
+            <ChartBar key={index} value={item} />
+          ))}
         </div>
       </div>
     </div>
   );
+}
+
+function calcNormalized(values: number[]): number[] {
+  const nonZeroData = values.filter((item) => item !== 0);
+  const minValue = Math.min(...nonZeroData);
+  const maxValue = Math.max(...nonZeroData);
+  const interval = maxValue - minValue;
+
+  return values.map(item => {
+    if (item === 0) return 2;
+    const increment = ((item - minValue) * HEIGHT_DIFF) / interval;
+    return MIN_HEIGHT + Math.round(increment);
+  })
 }
