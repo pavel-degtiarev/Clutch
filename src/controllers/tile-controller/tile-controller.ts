72986d20f13341c33b9f597c8390d13c9862ec9c
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-07-27 21:58:26 +0700

    TileController provides correct ChartData

--- src/controllers/tile-controller/tile-controller.ts
+++ src/controllers/tile-controller/tile-controller.ts
@@ -21,7 +21,23 @@ export default abstract class TileController<T extends StatisticsFields> {
 
   setTileLegend(statStore: StatRecord[]): TileData {
     const currentMonthStat = statStore[statStore.length - 1];
-    return { ...this.tile, value: currentMonthStat.value };
+    let chartData = statStore.map((item) => item.value);
+    
+    // нужны данные за полгода (statStore.length = 6).
+    // если не хватает, в начало массива дописываем нули.
+    // если много, берем последние 6 элементов.
+    switch (true) {
+      case chartData.length > 6:
+        chartData = chartData.slice(-6);
+        break;
+      
+      case chartData.length < 6:
+        const zeros = (new Array(6 - chartData.length)).fill(0);
+        chartData = zeros.concat(chartData);
+        break;
+    }
+
+    return { ...this.tile, value: currentMonthStat.value, chartData: chartData };
   }
 
   abstract initController(store: ClutchStoreType): void;
