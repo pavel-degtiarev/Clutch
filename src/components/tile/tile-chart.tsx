907274a54038a05a0cea0d0cadb73995a7198e77
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-07-27 21:21:03 +0700

    Chart refactored on plain <div>

--- src/components/tile/tile-chart.tsx
+++ src/components/tile/tile-chart.tsx
@@ -1,42 +1,28 @@
 import * as React from "react";
-import { BarChart, Bar, ResponsiveContainer, ReferenceLine, Label } from "recharts";
 import classNames from "classnames";
 
 import tileStyles from "./tile.module.scss";
 import styles from "./tile-chart.module.scss";
 import { TileChartRecord } from "../../controllers/tiles-controller/tiles-controller";
 
-function RoundedBar({ fill, x, y, width, height }: any) {
-  return (
-    <rect
-      x={x} y={y} width={width} height={height}
-      rx={width / 2} ry={width / 2}
-      stroke="none" fill={fill}
-    />
-  );
-}
-
 type TileChartProps = {
   data: TileChartRecord[];
 };
 
 export default function TileChart({ data }: TileChartProps) {
-  // const allValues = data.map(item => item.value);
-  // const avg = Math.round(allValues.reduce((accum, item) => accum + item, 0) / allValues.length);
-
   return (
     <div className={classNames(tileStyles.data, styles.chart)}>
-      <ResponsiveContainer width="100%" height="100%">
-        <BarChart data={data} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
-          <Bar dataKey="value" fill="#3b9cbc" shape={<RoundedBar />} isAnimationActive={false} />
-          {/* <ReferenceLine y={avg}
-            stroke="#88e5d4" strokeDasharray="1 2" isFront
-            label={
-              <Label value={`средн. ${avg}`} position="insideTopRight" fontSize={10} fill="white" />
-            }
-          /> */}
-        </BarChart>
-      </ResponsiveContainer>
+      <div className={classNames(styles.chartContainer)}>
+        <div className={classNames(styles.barsContainer)}>
+          <div className={classNames(styles.bar)} style={{ height: "65%" }}></div>
+          <div className={classNames(styles.bar)} style={{ height: "40%" }}></div>
+          <div className={classNames(styles.bar)} style={{ height: "0%" }}></div>
+          <div className={classNames(styles.bar)} style={{ height: "20%" }}></div>
+          <div className={classNames(styles.bar)} style={{ height: "80%" }}></div>
+          <div className={classNames(styles.bar)} style={{ height: "100%" }}></div>
+          <div className={classNames(styles.bar)} style={{ height: "85%" }}></div>
+        </div>
+      </div>
     </div>
   );
 }
