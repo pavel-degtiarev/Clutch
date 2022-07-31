dff257648f3b2e6512d00fb8e4f6d1ff405533f0
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-07-31 11:33:42 +0700

    timeTabs transferred to App

--- src/general/app.tsx
+++ src/general/app.tsx
@@ -4,14 +4,13 @@ import styles from "../styles/global.module.scss";
 import Header from "../modules/header/Header";
 import Main from "../modules/main-container/main-container";
 import Reminder from "../modules/reminder/reminder";
-import TabsGroup from "../components/tabs/tabs-group";
+import TabsGroup, { TabInfo } from "../components/tabs/tabs-group";
 import Tiles from "../modules/tiles/tiles";
 import PopupSwitch from "../components/popup-switch/popup-switch";
 import FormState from "../context/form-state/form-state";
 import FormDisplayState from "../context/form-display/form-display-state";
 
 import reminders from "../mocks/reminders";
-import { timeTabs } from "../mocks/tabs";
 import { forms } from "./forms";
 
 import { ClutchStoreType, useClutchStoreDispatch, useClutchStoreSelector } from "../store/store";
@@ -19,6 +18,11 @@ import { useStore } from "react-redux";
 import { tilesController } from "../index";
 
 // ===========================================
+
+const timeTabs: TabInfo[] = [
+  { id: "month", title: "Месяц" },
+  { id: "year", title: "Год" },
+];
 
 export default function App() {
   // const storeDispatch = useClutchStoreDispatch();
