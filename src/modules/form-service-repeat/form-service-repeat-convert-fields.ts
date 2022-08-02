60206b9cea8f087fa2fa4b665228e73f629c783a
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-08-02 17:06:16 +0700

    Service and Repeat forms convertation changed

--- src/modules/form-service-repeat/form-service-repeat-convert-fields.ts
+++ src/modules/form-service-repeat/form-service-repeat-convert-fields.ts
@@ -1,19 +1,15 @@
 import { RepeatFormState } from "../../context/form-state/form-init-states";
-import { repeatFormFinalState } from "../../HOC/with-validate-check/check-form";
+import { RepeatFormFinalState } from "../../HOC/with-validate-check/check-form";
 
-export function convertServiceRepeatFields(
-  state: RepeatFormState): repeatFormFinalState {
-  
-  const finalState: repeatFormFinalState =
-    state.repeatByRun || state.repeatByTime
-      ? {
-          repeatByRun: state.repeatByRun,
-          repeatingRun: Number(state.repeatingRun),
-          repeatByTime: state.repeatByTime,
-          repeatingTime: Number(state.repeatingTime),
-          repeatTimeSlot: state.repeatTimeSlot,
-        }
-      : null;
+export function convertServiceRepeatFields(state: RepeatFormState): RepeatFormFinalState {
+  const finalState: RepeatFormFinalState = {
+    serviceId: 0,
+    repeatByRun: state.repeatByRun,
+    repeatingRun: Number(state.repeatingRun),
+    repeatByTime: state.repeatByTime,
+    repeatingTime: Number(state.repeatingTime),
+    repeatTimeSlot: state.repeatTimeSlot,
+  };
 
   return finalState;
 }
