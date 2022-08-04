7f851576eb3144f3feae15e60d719e20b1c0045b
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-08-04 21:29:47 +0700

    BurgerMenu has controls and having been showed and  hide

--- src/components/burger-menu/burger-menu.tsx
+++ src/components/burger-menu/burger-menu.tsx
@@ -4,6 +4,7 @@ import Button from "../button/button";
 import FieldText from "../field/field-text";
 
 import styles from "./burger-menu.module.scss";
+import typeStyles from "../../styles/typography.module.scss";
 
 interface ContainerProps {
   isClosed: boolean;
@@ -13,7 +14,9 @@ interface ContainerProps {
 function BurgerMenuContainer({ isClosed, children }: ContainerProps) {
   return (
     <section className={styles.burgerMenu}>
-      <div className={classNames(styles.container, { [styles.closed]: isClosed })}>{children}</div>
+      <div className={classNames(styles.container, { [styles.burgerMenuClosed]: isClosed })}>
+        {children}
+      </div>
     </section>
   );
 }
@@ -25,8 +28,15 @@ interface BurgerMenuProps {
 export default function BurgerMenu({ isClosed }: BurgerMenuProps) {
   return (
     <BurgerMenuContainer isClosed={isClosed}>
-      <FieldText value="Honda FIT" name="car-name" label="Марка машины" />
-      <Button title="Удалить данные" clickHandler={() => {}} />
+
+      <div className={styles.inputContainer}>
+        <h2 className={classNames(typeStyles.titleNormal, styles.header)}>Марка машины</h2>
+        <FieldText value="" name="car-name" label="" auxStyles={styles.burgerMenuInput} />
+      </div>
+
+      <Button title="Удалить данные" auxStyles={styles.destroyDataButton}
+        clickHandler={() => { }} />
+
     </BurgerMenuContainer>
   );
 }
