345d1c08f9b6473ffb167d844f6378d518d4d09c
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-08-04 20:36:31 +0700

    BurgerMenu transformed into component

--- src/components/burger-menu/burger-menu.tsx
+++ src/components/burger-menu/burger-menu.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import classNames from "classnames";
+import React, { ReactNode } from "react";
+import Button from "../button/button";
+import FieldText from "../field/field-text";
 
 import styles from "./burger-menu.module.scss";
 
-export default function BurgerMenu() {
+interface ContainerProps {
+  isClosed: boolean;
+  children: ReactNode;
+}
+
+function BurgerMenuContainer({ isClosed, children }: ContainerProps) {
   return (
     <section className={styles.burgerMenu}>
-      <div className={styles.container}>
-        <input className="field no-wrap" name="car-name" value="Honda FIT" />
-
-        <button type="button" className="button button-main">
-          <span>Удалить данные</span>
-        </button>
-      </div>
+      <div className={classNames(styles.container, { [styles.closed]: isClosed })}>{children}</div>
     </section>
+  );
+}
+
+interface BurgerMenuProps {
+  isClosed: boolean;
+}
+
+export default function BurgerMenu({ isClosed }: BurgerMenuProps) {
+  return (
+    <BurgerMenuContainer isClosed={isClosed}>
+      <FieldText value="Honda FIT" name="car-name" label="Марка машины" />
+      <Button title="Удалить данные" clickHandler={() => {}} />
+    </BurgerMenuContainer>
   );
 }
