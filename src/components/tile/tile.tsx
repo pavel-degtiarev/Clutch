c65a2f492789feb424785f8f219156164d5f0e9d
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-08-08 13:16:01 +0700

    Tile component uses CurrentPageContext to switch to Stats

--- src/components/tile/tile.tsx
+++ src/components/tile/tile.tsx
@@ -1,15 +1,18 @@
-import * as React from "react";
+import React, { useContext } from "react";
 import TileHeader from "./tile-header";
 import TileChart from "./tile-chart";
 import { TileData } from "../../controllers/tiles-controller/tiles-controller";
+import { CurrentPageContext } from "../../context/current-page/current-page-context";
 
 import styles from "./tile.module.scss";
 
 type TileProps = TileData;
 
 export default function Tile({ title, units, value, chartData }: TileProps) {
+  const { switchToStat } = useContext(CurrentPageContext);
+
   return (
-    <div className={styles.tile}>
+    <div className={styles.tile} onClick={switchToStat}>
       <TileHeader title={title} units={units} value={value} />
       <TileChart chartData={chartData} />
     </div>
