24afc529fff44ef241512bfeaa77515da4f62ac0
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-08-08 13:13:15 +0700

    MainContainer uses CurrentPageContext for conditionally render either Tiles of Stats

--- src/general/app.tsx
+++ src/general/app.tsx
@@ -2,36 +2,18 @@ import React from "react";
 import styles from "../styles/global.module.scss";
 
 import Header from "../modules/header/Header";
-import Main from "../modules/main-container/main-container";
-import Reminder from "../modules/reminder/reminder";
-import TabsGroup, { TabInfo } from "../components/tabs/tabs-group";
-import Tiles from "../modules/tiles/tiles";
-import PopupSwitch from "../components/popup-switch/popup-switch";
-import FormState from "../context/form-state/form-state";
-import FormDisplayState from "../context/form-display/form-display-state";
-
-import { forms } from "./forms";
+import MainContainer from "../modules/main-container/main-container";
+import CurrentPage from "../context/current-page/current-page-context";
 
 import { remindersController, tilesController, titleController } from "../index";
-import TabsGroupContext from "../components/tabs/tabs-group-context";
 import { deleteClutchDb } from "../API/init-db";
 
 // ===========================================
 
-export enum TimeInterval {
-  MONTH = "month",
-  YEAR = "year",
-}
-
-const timeTabs: TabInfo[] = [
-  { id: TimeInterval.MONTH, title: "Месяц" },
-  { id: TimeInterval.YEAR, title: "Год" },
-];
-
 function deleteData(): void {
   const deleteMsg = `Удалить все данные?
   Эта операция необратима!`;
-  
+
   if (window.confirm(deleteMsg)) {
     titleController.clearTitle();
     tilesController.clearTiles();
@@ -45,22 +27,10 @@ export default function App() {
     <>
       <h1 className={styles.visuallyHidden}>Clutch. Car expenses logbook.</h1>
 
-      <Header controller={titleController} onDeleteData={deleteData} />
-
-      <Main>
-        <FormState>
-          <FormDisplayState>
-            <Reminder remindersController={remindersController} />
-
-            <TabsGroupContext tabInfo={timeTabs}>
-              <TabsGroup name="time-interval" />
-              <Tiles tilesController={tilesController} />
-            </TabsGroupContext>
-
-            <PopupSwitch forms={forms} />
-          </FormDisplayState>
-        </FormState>
-      </Main>
+      <CurrentPage>
+        <Header controller={titleController} onDeleteData={deleteData}/>
+        <MainContainer />
+      </CurrentPage>
     </>
   );
 }
--- src/modules/main-container/main-container.tsx
+++ src/modules/main-container/main-container.tsx
@@ -1,14 +1,52 @@
-import React, { ReactNode } from "react";
+import React, { useContext } from "react";
+import { TimeInterval } from "../../general/global.var";
+import { remindersController, tilesController } from "../../index";
+import { CurrentPageContext, Pages } from "../../context/current-page/current-page-context";
+import FormDisplayState from "../../context/form-display/form-display-state";
+import FormState from "../../context/form-state/form-state";
+import Reminder from "../reminder/reminder";
+import TabsGroupContext from "../../components/tabs/tabs-group-context";
+import TabsGroup, { TabInfo } from "../../components/tabs/tabs-group";
+import Tiles from "../tiles/tiles";
+import PopupSwitch from "../../components/popup-switch/popup-switch";
+import { forms } from "../../general/forms";
+
 import styles from "./main-container.module.scss";
 
-type MainProps = {
-  children: ReactNode;
-};
+const timeTabs: TabInfo[] = [
+  { id: TimeInterval.MONTH, title: "Месяц" },
+  { id: TimeInterval.YEAR, title: "Год" },
+];
 
-export default function Main({ children }: MainProps) {
+function TilesPage() {
+  return (
+    <FormState>
+      <FormDisplayState>
+        <Reminder remindersController={remindersController} />
+
+        <TabsGroupContext tabInfo={timeTabs}>
+          <TabsGroup name="time-interval" />
+          <Tiles tilesController={tilesController} />
+        </TabsGroupContext>
+
+        <PopupSwitch forms={forms} />
+      </FormDisplayState>
+    </FormState>
+  );
+}
+
+function StatPage() {
+  return <>STAT</>;
+}
+
+export default function MainContainer() {
+  const { currentPage } = useContext(CurrentPageContext);
+
   return (
     <main>
-      <div className={styles.container}>{children}</div>
+      <div className={styles.container}>
+        {currentPage === Pages.TILES ? <TilesPage /> : <StatPage />}
+      </div>
     </main>
   );
 }
