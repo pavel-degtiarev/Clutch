af5924819234ef09285b016d895af68529674ca8
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-08-09 11:36:25 +0700

    StatPage and TilesPage use refactored TabsWithContext

--- src/modules/page-tiles/page-tiles.tsx
+++ src/modules/page-tiles/page-tiles.tsx
@@ -4,8 +4,7 @@ import { remindersController, tilesController } from "../../index";
 import FormDisplayState from "../../context/form-display/form-display-state";
 import FormState from "../../context/form-state/form-state";
 import Reminder from "../reminder/reminder";
-import TabsGroupContext from "../../components/tabs/tabs-group-context";
-import TabsGroup from "../../components/tabs/tabs-group";
+import TabsWithContext from "../../components/tabs/tabs-group-context";
 import Tiles from "../tiles/tiles";
 import PopupSwitch from "../../components/popup-switch/popup-switch";
 import { forms } from "../../general/forms";
@@ -16,10 +15,9 @@ export default function PageTiles() {
       <FormDisplayState>
         <Reminder remindersController={remindersController} />
 
-        <TabsGroupContext tabInfo={timeTabs}>
-          <TabsGroup name="time-interval" />
+        <TabsWithContext name="time-interval" tabInfo={timeTabs}>
           <Tiles tilesController={tilesController} />
-        </TabsGroupContext>
+        </TabsWithContext>
 
         <PopupSwitch forms={forms} />
       </FormDisplayState>
