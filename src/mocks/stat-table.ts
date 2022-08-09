1f77f0605daef5b1bf32a08b44298736cff83e76
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-08-09 16:53:42 +0700

    StatTable changes

--- src/mocks/stat-table.ts
+++ src/mocks/stat-table.ts
@@ -6,9 +6,7 @@ export const statTableData: StatSlot[] = [
     rows: [
       { title: "Пробег", value: "1500 км." },
       { title: "Расход топлива", value: "7,5 л./100 км." },
-      { title: "Затраты на топливо", value: "2500 руб." },
-      { title: "Сервис и расходники", value: "5500 руб." },
-      { title: "Прочие затраты", value: null },
+      { title: "Затраты", value: null },
     ],
   },
   {
@@ -16,9 +14,7 @@ export const statTableData: StatSlot[] = [
     rows: [
       { title: "Пробег", value: "2500 км." },
       { title: "Расход топлива", value: "7,1 л./100 км." },
-      { title: "Затраты на топливо", value: "5200 руб." },
-      { title: "Сервис и расходники", value: null },
-      { title: "Прочие затраты", value: null },
+      { title: "Затраты", value: null },
     ],
   },
   {
@@ -26,9 +22,7 @@ export const statTableData: StatSlot[] = [
     rows: [
       { title: "Пробег", value: "2000 км." },
       { title: "Расход топлива", value: "7,8 л./100 км." },
-      { title: "Затраты на топливо", value: "4100 руб." },
-      { title: "Сервис и расходники", value: null },
-      { title: "Прочие затраты", value: null },
+      { title: "Затраты", value: null },
     ],
   },
 ];
