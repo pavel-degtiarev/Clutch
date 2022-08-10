894ada2cefa939d079dc36130cba8a621d3cf609
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-08-10 15:10:01 +0700

    Table extracted to stateless component

--- src/components/stat-table/stat-table.tsx
+++ src/components/stat-table/stat-table.tsx
@@ -2,10 +2,7 @@ import React, { useContext } from "react";
 import useStatTable from "../../hooks/use-stat-table";
 import { Statistics } from "../../store/stat-slice/stat-slice";
 import { TabsContext } from "../tabs/tabs-group-context";
-import { StatSlot } from "./slot-types";
-import TableSlot from "./slot/slot";
-
-import styles from "./stat-table.module.scss";
+import Table from "./table";
 
 interface StatTableProps {
   stat: Statistics;
@@ -14,12 +11,6 @@ interface StatTableProps {
 export default function StatTable({ stat }: StatTableProps) {
   const timeIntervalTab = useContext(TabsContext);
   const slots = useStatTable(stat, timeIntervalTab);
-  
-  return slots.length > 0 ? (
-    <section className={styles.statTable}>
-      {slots.map((slot, index) => (
-        <TableSlot key={index} header={slot.header} rows={slot.rows} />
-      ))}
-    </section>
-  ) : null;
+
+  return slots.length > 0 ? <Table slots={slots} /> : null;
 }
