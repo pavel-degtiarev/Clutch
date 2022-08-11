11ede6feb5e3e2655a081958ec5e070c4d5bff10
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-08-11 14:48:34 +0700

    RawTable gets FormContext and passes it to FormEditController

--- src/components/stat-table/raw-table.tsx
+++ src/components/stat-table/raw-table.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { FormStateContext } from "../../context/form-state/form-state";
 import useRawData from "../../hooks/use-raw-data";
 import Table from "./table";
+import { formEditController } from "../../index";
+import { FormDisplayContext } from "../../context/form-display/form-display-state";
+import { FinalBasicFormsState } from "../../HOC/with-validate-check/check-form";
 
 export default function RawTable() {
+  const { showForm } = useContext(FormDisplayContext);
+  const formState = useContext(FormStateContext);
+  formEditController.setShowFormAction(showForm);
+  formEditController.setFormsDataActions(formState);
+
   const data = useRawData();
 
-  return <Table slots={data} />;
+  return (
+    <Table
+      slots={data}
+      slotEditHandler={(formData: FinalBasicFormsState) => formEditController.editForm(formData)}
+    />
+  );
 }
