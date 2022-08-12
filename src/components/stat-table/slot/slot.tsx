2863f4d10e7d51e368809e2670581b65ea496fd4
Pavel Degtiarev <pavel.degtiarev@gmail.com>
2022-08-12 19:11:52 +0700

    deleteHandler added to props

--- src/components/stat-table/slot/slot.tsx
+++ src/components/stat-table/slot/slot.tsx
@@ -5,7 +5,6 @@ import SlotRow from "./slot-row";
 import { StatSlot } from "../slot-types";
 import { FinalBasicFormsStateWithID } from "../../../HOC/with-validate-check/check-form";
 import { RowDeletable } from "../../details-list/row-deletable";
-import { formEditController } from "../../../index";
 
 interface RowProps {
   deletable: boolean;
@@ -25,18 +24,25 @@ function Row({ deletable, onDelete, children }: RowProps) {
 
 interface TableSlotProps extends StatSlot {
   slotEditHandler: (auxData: FinalBasicFormsStateWithID) => void;
+  slotDeleteHandler: (auxData: FinalBasicFormsStateWithID) => void;
   rowDeletable: boolean;
 }
 
-export default function TableSlot({ header, rows, slotEditHandler, rowDeletable }: TableSlotProps) {
+export default function TableSlot({
+  header,
+  rows,
+  slotEditHandler,
+  slotDeleteHandler,
+  rowDeletable,
+}: TableSlotProps) {  
   return (
     <div className={styles.slot}>
       <SlotHeader header={header} />
       {rows.map((row, index) => (
         <Row
           deletable={rowDeletable}
-          key={index}
-          onDelete={() => row.aux && formEditController.deleteRow(row.aux)}>
+          key={`${row.title}-${row.aux?.id}` || index}
+          onDelete={() => row.aux && slotDeleteHandler(row.aux)}>
           <SlotRow
             title={row.title}
             value={row.value}
--- src/components/stat-table/table.tsx
+++ src/components/stat-table/table.tsx
@@ -7,22 +7,25 @@ import styles from "./stat-table.module.scss";
 interface TableProps {
   slots: StatSlot[];
   slotEditHandler?: (formData: FinalBasicFormsStateWithID) => void;
+  slotDeleteHandler?: (formData: FinalBasicFormsStateWithID) => void;
   rowsDeletable?: boolean;
 }
 
 export default function Table({
   slots,
   slotEditHandler = () => {},
+  slotDeleteHandler = () => {},
   rowsDeletable = false,
 }: TableProps) {
   return (
     <section className={styles.statTable}>
-      {slots.map((slot, index) => (
+      {slots.map((slot) => (
         <TableSlot
-          key={index}
+          key={slot.header}
           header={slot.header}
           rows={slot.rows}
           slotEditHandler={slotEditHandler}
+          slotDeleteHandler={slotDeleteHandler}
           rowDeletable={rowsDeletable}
         />
       ))}
